4efdafac31ec467697dd1e0ede7cccc14165bdda
Zixuan <liusherry08@gmail.com>
2025-11-09 23:38:14 +0000

    added form layout

--- releaf-app/src/pages/LoginPage.jsx
+++ releaf-app/src/pages/LoginPage.jsx
@@ -1,9 +1,15 @@
 import HomePageLayout from "../components/HomePageLayout";
+import { useLocation } from "react-router-dom";
+import NewUserForm from "../components/NewUserForm";
 
 export default function SignupPage() {
+  const location = useLocation();
+  console.log(location.pathname);
   return (
     <div className=" w-full flex-grow">
-      <HomePageLayout></HomePageLayout>
+      <HomePageLayout>
+        {location.pathname === "/login" && <NewUserForm />}
+      </HomePageLayout>
     </div>
   );
 }
--- releaf-app/src/pages/SignupPage.jsx
+++ releaf-app/src/pages/SignupPage.jsx
@@ -1,9 +1,16 @@
 import HomePageLayout from "../components/HomePageLayout";
+import { useLocation } from "react-router-dom";
+import NewUserForm from "../components/NewUserForm";
 
 export default function SignupPage() {
+  const location = useLocation();
+  console.log(location.pathname);
+
   return (
     <div className=" w-full flex-grow">
-      <HomePageLayout></HomePageLayout>
+      <HomePageLayout>
+        {location.pathname === "/signup" && <NewUserForm />}
+      </HomePageLayout>
     </div>
   );
 }
