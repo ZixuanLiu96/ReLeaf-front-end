d48e915456d6fc89ce33381ea24c2455ea43d89e
Zixuan <liusherry08@gmail.com>
2025-11-25 23:22:57 +0000

    added about page

--- releaf-app/src/pages/AboutPage.jsx
+++ releaf-app/src/pages/AboutPage.jsx
@@ -4,6 +4,25 @@ export default function AboutPage() {
   return (
     <div className="flex-grow">
       <UserNavBar />
+      <div className="mt-40 p-10 flex flex-col justify-center gap-10">
+        <h1 className="text-4xl font-bold">How to use this app?</h1>
+        <div className="flex items-center gap-20">
+          <div className="p-4 border-2 border-[#c86e59] rounded-md w-1/2 text-lg">
+            <p>
+              After logging into the app, you can see all the plants that have
+              been published on the homepage, including both unadopted and
+              adopted ones. Users can select plants they are interested in and
+              submit an adoption request. After the background review, the
+              adoption will be successful. If a user wants to initiate an
+              adoption, they can also fill in the plant information on the "give
+              away plants" page. After the background review, the user's plant
+              will be published. Users can also modify their personal
+              information.
+            </p>
+          </div>
+          <div className="w-85 h-85 bg-[url(/book-bg.png)] bg-center bg-cover"></div>
+        </div>
+      </div>
     </div>
   );
 }
--- releaf-app/src/pages/ProfilePage.jsx
+++ releaf-app/src/pages/ProfilePage.jsx
@@ -1,10 +1,13 @@
 import UserNavBar from "../components/UserNavBar";
 import { PlusOutlined } from "@ant-design/icons";
 import { Button, Form, Input, Upload } from "antd";
+import { AuthContext } from "../contexts/auth.context";
+import { useContext } from "react";
 
 const { TextArea } = Input;
 
 export default function ProfilePage() {
+  const { user } = useContext(AuthContext);
   const [form] = Form.useForm();
   return (
     <div className="flex-grow">
